ac38a3f39a4a1d63e30bf6d37f3c00830be7db24
shivamjha1 <shivam.21b0101002@abes.ac.in>
2024-04-05 22:50:32 +0530

    x

--- store/models.py
+++ store/models.py
@@ -7,6 +7,12 @@ class Author(models.Model):
     first_name=models.CharField(max_length=50)
     last_name=models.CharField(max_length=50)
     
+    def full_name(self):
+    
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.full_name()
+    
 class Book(models.Model):
     title=models.CharField(max_length=50)
     rating=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
